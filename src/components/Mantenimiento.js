75e5f63850a55df9af1d980df5ad530f03caf638
Sebas <sperezb@uteq.edu.ec>
2024-09-23 04:42:20 -0500

    Escoger discos

--- src/components/Mantenimiento.js
+++ src/components/Mantenimiento.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, Spinner } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import axios from 'axios';
 import io from 'socket.io-client'; 
 
 const MySwal = withReactContent(Swal);
@@ -11,29 +12,15 @@ const Mantenimiento = () => {
   const [alerts, setAlerts] = useState([]);
   const [definitions, setDefinitions] = useState([]);
   const [disk, setDisk] = useState([]);
+  const [smartDevices, setSmartDevices] = useState([]); 
+  const [selectedDisk, setSelectedDisk] = useState(null); 
 
   const [diskUsagePercent, setDiskUsagePercent] = useState(0);
   const [diskErrors, setDiskErrors] = useState(0);
-   
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
-  const alertIcons = {
-    'critical': 'bi bi-exclamation-triangle',
-    'ok': 'bi bi-check-circle',
-  };
-
-  const choose_disk = async(diskEscogido)=> 
-  {
-    const response = await axios.post("http://127.0.0.1:5000/api/choose_devices", diskEscogido);
-  
-  }
-
-  useEffect(async () => {
-    const response = await axios.get("http://127.0.0.1:5000/api/get_devices");
-    setDisk(response.data.devices);
-
-
+  const socketIO = () => {
     socket.on('connect', () => {
       console.log('Conectado al servidor Socket.IO');
     });
@@ -62,7 +49,6 @@ const Mantenimiento = () => {
           icon: 'error',
           confirmButtonText: 'Aceptar',
         });
-        // Marcar el estado de error y dejar de cargar
         setIsError(true);
         setIsLoading(false);
       }
@@ -71,9 +57,30 @@ const Mantenimiento = () => {
     return () => {
       socket.off('new_alert');
     };
+  };
+
+  const choose_disk = async (diskEscogido) => {
+    setSelectedDisk(diskEscogido); 
+    const response = await axios.post("http://127.0.0.1:5000/api/choose_devices", { name: diskEscogido });
+    socketIO();
+  };
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:5000/api/get_devices");
+        setDisk(response.data.devices);
+        setSmartDevices(response.data.smartctl_devices);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching devices:', error);
+      }
+    };
+
+    fetchDevices();
   }, []);
 
-  const handleNewAlert = async (newAlerts, definitions) => { 
+  const handleNewAlert = async (newAlerts, definitions) => {
     let startIndex = 0;
   
     while (startIndex < newAlerts.length) {
@@ -101,10 +108,10 @@ const Mantenimiento = () => {
     <Container className="mt-5">
       <Row>
         <Col>
-          <h1 className="text-center">Monitoreo de Almacenamiento</h1>
+          <h1 className="text-center">Seleccionar Disco</h1>
         </Col>
       </Row>
- 
+
       {isLoading ? (
         <Row className="justify-content-center mt-5">
           <Spinner animation="border" role="status" variant="primary">
@@ -113,46 +120,78 @@ const Mantenimiento = () => {
         </Row>
       ) : isError ? (
         <Row className="justify-content-center mt-5">
-          <p className="text-center text-danger">Ha ocurrido un error al obtener los datos del disco.</p>
+          <p className="text-center text-danger">Ha ocurrido un error al obtener los discos.</p>
         </Row>
       ) : (
+        <Row className="mt-4">
+          {disk.length === 0 ? (
+            <Col>
+              <p>No hay discos disponibles.</p>
+            </Col>
+          ) : (
+            disk.map((diskItem, index) => (
+              <Col md={4} key={index}>
+                <Card className="mb-3" onClick={() => choose_disk(smartDevices[index])} style={{ cursor: 'pointer' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      <i className="bi bi-hdd-fill me-2"></i>  
+                      {diskItem.filesystem}
+                    </Card.Title>
+                    <Card.Text>
+                      Tamaño del disco: {diskItem.size} 
+                    </Card.Text>
+                    <Card.Text>
+                      Espacio utilizado: {diskItem.used} 
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
+        </Row>
+      )}
+
+  
+      {selectedDisk && (
         <>
           <Row>
+            <Col>
+              <h1 className="text-center">Monitoreo de Almacenamiento</h1>
+            </Col>
+          </Row>
+        
+          <Row>
             <Col md={6}>
-              <Card className={`bg-info text-white mb-3`}>
+              <Card className="bg-info text-white mb-3">
                 <Card.Body>
                   <Card.Title>
-                    <i className="bi bi-hdd-fill me-2"></i> 
+                    <i className="bi bi-hdd-fill me-2"></i>
                     Almacenamiento del Disco
-                  </Card.Title> 
-                  <Card.Text>
-                       {diskUsagePercent}
-                  </Card.Text>
+                  </Card.Title>
+                  <Card.Text>{diskUsagePercent}</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={6}>
-              <Card className={`bg-warning text-dark mb-3`}>
+              <Card className="bg-warning text-dark mb-3">
                 <Card.Body>
                   <Card.Title>
-                    <i className="bi bi-exclamation-triangle-fill me-2"></i>  
+                    <i className="bi bi-exclamation-triangle-fill me-2"></i>
                     Errores en el Disco
                   </Card.Title>
                   <Card.Text>
-                  {diskErrors > 0 
-                      ? {diskErrors}
-                      :  `El disco no tiene errores`}   
-                      <br>
-                      </br>
-                  Tasa de detección automática de errores: 98%
+                    {diskErrors > 0 ? `${diskErrors}` : `El disco no tiene errores`} 
+                    <br />
+                    Tasa de detección automática de errores: 98%
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
+
           <Row className="mt-4">
             <Col>
-              <h3 className="text-left">Propiedades del disco obtenidos por la función SMART</h3>
+              <h3 className="text-left">Propiedades del disco obtenidas por la función SMART</h3>
             </Col>
           </Row>
 
@@ -165,12 +204,12 @@ const Mantenimiento = () => {
               alerts.map((alert, index) => {
                 const estado = alert.includes("Estado: Crítico") ? "Crítico" : "Buen estado";
                 const alertClass = estado === "Buen estado" ? "bg-success text-white" : "bg-danger text-white";
-                const icon = estado === "Buen estado" ? alertIcons['ok'] : alertIcons['critical'];
+                const icon = estado === "Buen estado" ? 'bi-check-circle-fill' : 'bi-exclamation-triangle-fill';
                 return (
                   <Card className={`${alertClass} mb-3`} key={index}>
                     <Card.Body>
                       <Card.Title>
-                        <i className={`${icon} me-2`}></i>
+                        <i className={`bi ${icon} me-2`}></i>
                         {alert}
                       </Card.Title>
                     </Card.Body>
